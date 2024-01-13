cfac0bc4ad176f40a917ff6b302b35a05be021cf
DatTran <trancongdat.datct@gmail.com>
2024-01-13 17:22:25 +0700

    add connect to db

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,3 +3,31 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+// 035uENtaHHBenwsG
+
+const MONGO_URI =
+  'mongodb+srv://trancongdat2117:035uENtaHHBenwsG@cluster0-dattran.f5g38ks.mongodb.net/?retryWrites=true&w=majority'
+
+const DATABASE_NAME = 'trancongdat2117'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+let trelloDb = null
+
+const mongoClientInstance = new MongoClient(MONGO_URI, {
+  serverApi: ServerApiVersion.v1,
+  strict: true,
+  deprecated: true
+})
+
+export const CONNECT_DB = async () => {
+  await mongoClientInstance.connect()
+  trelloDb = mongoClientInstance.db(DATABASE_NAME)
+}
+
+export const GET_DB = () => {
+  if (!trelloDb) {
+    throw new Error('GET_DB_ERROR')
+  }
+  return trelloDb
+}
