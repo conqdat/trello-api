70bff404f2b129d3d3fd59461920e8e109898398
DatTran <trancongdat.datct@gmail.com>
2024-01-17 23:34:50 +0700

    add set up router

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,3 +3,16 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+import express from 'express'
+
+const router = express.Router()
+
+router.use('/status', (req, res) => {
+  res.status(200).json({
+    status: 'OK',
+    message: 'API is running normally.'
+  })
+})
+
+export const APIs_V1 = router
--- src/server.js
+++ src/server.js
@@ -9,13 +9,12 @@ import express from 'express'
 import exitHook from 'async-exit-hook'
 import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
+import { APIs_V1 } from '~/routes/v1'
 
 const START_SERVER = () => {
   const app = express()
 
-  app.get('/', (req, res) => {
-    res.end('<h1>Hello World!</h1><hr>')
-  })
+  app.use('/api/v1', APIs_V1)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     // eslint-disable-next-line no-console
